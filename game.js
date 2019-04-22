1b6d69f37a61e7e2836a592e7a140edb0cbcfc69
zhuang-jie <1450482096@qq.com>
2019-04-22 16:45:16 +0800

    push~~

---
+++ game.js
@@ -0,0 +1,15 @@
+/**
+ * author：庄楚杰
+ * phone|wx：15089514341
+ * email：15089514341@163.com
+ * version：1.0.0
+ * Update：2017-12
+ * 文明转载，勿去掉作者信息，谢谢！
+*/
+
+
+// import './js/libs/weapp-adapter'
+// import './js/libs/symbol'
+
+import {Main} from "./main.js";
+new Main();
